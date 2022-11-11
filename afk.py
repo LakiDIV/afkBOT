074b286e389b23cd27f6df0cbc460d3828ba275f
Lakshika Madhuranga <lakshikamadhuranga21@gmail.com>
2022-11-12 03:31:36 +0530

    Update afk.py
    Added win32api, and keyboard library functionalities

--- afk.py
+++ afk.py
@@ -1,11 +1,22 @@
 import pyautogui as pag
 import random
 import time
+import keyboard
+import win32api
 
-while True:
+use_win32API_movement = True
+
+# ! Need to hold down the key to stop
+while keyboard.is_pressed('q') == False:
     x = random.randint(600,700)
     y = random.randint(200,600)
-    speed = random.uniform(0.5, 1.0)
+    
     timer = random.uniform(2.0, 5.0)
-    pag.moveTo(x,y,speed)
+    if use_win32API_movement:
+        win32api.SetCursorPos((x,y))
+    else:
+        speed = random.uniform(0.5, 1.0)
+        pag.moveTo(x,y,speed)
+    
     time.sleep(timer)
+    
